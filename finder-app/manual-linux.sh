b6f45cd798c520b5a3245971aa642b8ead8b6a63
Krishna Suhagiya <krishna.suhagiya@colorado.edu>
2024-02-04 18:58:48 -0700

    Assignment 3_2: manual-linux.sh: Add mkdir return check and build logs
    Signed-off-by: Krishna Suhagiya <krishna.suhagiya@colorado.edu>

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -22,6 +22,11 @@ else
 fi
 
 mkdir -p ${OUTDIR}
+# Exit if the directory could not be created
+if [ $? -ne 0 ]; then
+    echo "Could not create ${OUTDIR}."
+    exit 1
+fi
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -35,10 +40,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # Kernel build steps
+    echo "Deep cleaning the kernel build tree"
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+    echo "Building device configuration"
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    echo "Building kernel image"
     make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+    #echo "Building kernel modules"
     #make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+    echo "Building device tree"
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
 fi
 
@@ -53,6 +63,7 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
+echo "Creating root filesystem"
 mkdir ${OUTDIR}/rootfs
 cd ${OUTDIR}/rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
@@ -65,14 +76,14 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # Configure busybox
+    echo "Configuring busybox"
     make distclean
     make defconfig
 else
     cd busybox
 fi
 
-# Make and install busybox
+echo "Building and installing busybox"
 make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE
 make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE install
 
@@ -81,24 +92,23 @@ cd ${OUTDIR}/rootfs
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+echo "Adding library dependencies to rootfs"
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 cp $SYSROOT/lib/ld-linux-aarch64.so.1 lib/
 cp $SYSROOT/lib64/libm.so.6 lib64/
 cp $SYSROOT/lib64/libresolv.so.2 lib64/
 cp $SYSROOT/lib64/libc.so.6 lib64/
 
-# TODO: Make device nodes
+echo "Making device nodes"
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 666 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+echo "Cross-compiling the writer utility"
 cd ${FINDER_APP_DIR}
 make clean
 make CROSS_COMPILE=$CROSS_COMPILE
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+echo "Copying the finder related scripts and executables to the /home directory on the target rootfs"
 cp finder.sh ${OUTDIR}/rootfs/home
 cp finder-test.sh ${OUTDIR}/rootfs/home
 cp writer ${OUTDIR}/rootfs/home
@@ -107,11 +117,11 @@ cp conf/username.txt ${OUTDIR}/rootfs/home/conf/
 cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf/
 cp autorun-qemu.sh ${OUTDIR}/rootfs/home
 
-# TODO: Chown the root directory
+echo "Changing the ownership of the root directory"
 cd ${OUTDIR}/rootfs
 sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
+echo "Creating initramfs.cpio.gz"
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
 cd ${OUTDIR}
 gzip -f initramfs.cpio
